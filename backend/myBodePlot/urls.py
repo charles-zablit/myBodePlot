c6e2a47282e34b8a372890a7a8d0d27af755f3d6
charles-zablit <zablitcharles@gmail.com>
2021-07-16 17:33:56 +0200

    static

--- backend/myBodePlot/urls.py
+++ backend/myBodePlot/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include
-from django.views.generic import TemplateView
+from django.urls import path, include, patterns
+from django.conf import settings
 
 urlpatterns = [
   path('admin/', admin.site.urls),
   path('', include('home.urls'))
 ]
+if not settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
