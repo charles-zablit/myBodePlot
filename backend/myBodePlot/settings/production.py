024a8d01c9cf7bb5e0c1bc1bc1e1be2dcb826b35
charles-zablit <zablitcharles@gmail.com>
2021-07-16 16:57:44 +0200

    logging

--- backend/myBodePlot/settings/production.py
+++ backend/myBodePlot/settings/production.py
@@ -5,7 +5,7 @@ import dj_database_url
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['my-bode-plot.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -20,7 +20,42 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
-#MIDDLEWARE.append('whitenoise.middleware.WhiteNoiseMiddleware')
+LOGGING = {
+	'version': 1,
+	'disable_existing_loggers': False,
+	'formatters': {
+		'verbose': {
+				'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
+										'pathname=%(pathname)s lineno=%(lineno)s ' +
+										'funcname=%(funcName)s %(message)s'),
+				'datefmt': '%Y-%m-%d %H:%M:%S'
+		},
+		'simple': {
+				'format': '%(levelname)s %(message)s'
+		}
+	},
+	'handlers': {
+		'null': {
+				'level': 'DEBUG',
+				'class': 'logging.NullHandler',
+		},
+		'console': {
+				'level': 'DEBUG',
+				'class': 'logging.StreamHandler',
+				'formatter': 'verbose'
+		}
+	},
+	'loggers': {
+		'testlogger': {
+				'handlers': ['console'],
+				'level': 'INFO',
+		}
+	}
+}
 
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+DEBUG_PROPAGATE_EXCEPTIONS = True
+
+MIDDLEWARE.append('whitenoise.middleware.WhiteNoiseMiddleware')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 django_heroku.settings(locals())
